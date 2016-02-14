c5b199ab7ede53788eefa1b5538b51956d4d2940
Tim Kinnel <tim_kinnel@wgbh.org>
2016-02-14 14:07:07 -0500

    Further fixes - synchronous submission of downloads

---
+++ do_it.php
@@ -0,0 +1,75 @@
+<?php  
+
+define('DL_FOLDER', 'downloads');
+define('DL_PATH', dirname( __FILE__ ) . DIRECTORY_SEPARATOR. DL_FOLDER . DIRECTORY_SEPARATOR);
+  
+function downloadable_files($f) {
+  return preg_match("/\.(m4a|mp4)$/", $f);
+}
+
+function add_folder($f) {
+  return array( 
+    "filepath" => DL_FOLDER . DIRECTORY_SEPARATOR . $f,
+    "filename" => $f,
+  );
+}
+
+switch ($_GET['action']) {
+  case 'get_downloadable_files':
+
+    if (is_dir(DL_PATH)) {
+      $mp4_files = array_values(array_filter(scandir(DL_PATH), "downloadable_files"));
+    }
+    $mp4_paths = array_map("add_folder", $mp4_files); 
+
+    echo json_encode($mp4_paths);
+    break;
+
+  case 'upload_and_convert':
+    if (!empty($_FILES)) {
+     
+      $tempFile = $_FILES['file']['tmp_name'];
+      $fileName = pathinfo($_FILES['file']['name']);
+      $targetFile =  DL_PATH . $fileName['filename']. ".m4a" ;
+
+      # Convert file to mp4 using ffmpeg
+      exec("ffmpeg -i $tempFile -c:a libfdk_aac $targetFile");       
+    }
+    break;
+    
+  case 'download_files':
+    $folder = "downloads_" . date("Ymd-Hi");
+    
+    $files = $_POST['files'];
+
+    if (isset($files) && sizeof($files) > 0) {
+      $file_list = '';
+      foreach ($files as $file) {
+        $file_list .= DL_FOLDER . "/$file ";
+      }
+      $tmp_folder = "/tmp/$folder";
+      $cmd = "mkdir $tmp_folder";
+      exec($cmd);
+  
+      $cmd = "cp $file_list $tmp_folder";
+      exec($cmd);
+  
+      $cmd = "cd /tmp; zip -r $folder $folder";
+      exec($cmd);
+      
+      $zipfile = $folder .".zip";
+
+      header('Content-Type: application/zip');
+      header("Content-Disposition: attachment; filename=" . $zipfile);
+      header("Content-Transfer-Encoding: binary");
+ 
+      readfile("/tmp/$zipfile");
+    }
+    break;
+    
+  case 'clear_files':
+    shell_exec("rm " . DL_PATH . "/*.m4a");
+    break;
+}
+
+?>
--- mp4download.js
+++ mp4download.js
@@ -5,38 +5,48 @@ app.controller("downloadsController", function($scope, $http, $interval) {
     $scope.toDownload = [];
   
     $scope.updateFileList = function() {
-      $http.get('downloadable.php')
+      $http.get('do_it.php?action=get_downloadable_files')
         .success(function(data) {
           $scope.availableFiles = data;
-        });
+         });
     };
     
-    $interval( function() { $scope.updateFileList(); }, 5000);
-
-    $scope.checkAll = function(e) {
-      e.preventDefault();
-      $scope.toDownload = $scope.availableFiles.map(function(item) item.filepath);
-      $scope.status = "All files selected.";
-    };
-
-    $scope.checkNone = function(e) {
-      e.preventDefault();
+//    $interval( function() { $scope.updateFileList(); }, 5000);
+    
+    $scope.clearFiles = function(e) {
       $scope.toDownload = [];
-      $scope.status = "Files to download cleared.";
+      $http.get('do_it.php?action=clear_files')
+        .success(function(data) {
+          $scope.updateFileList();
+          $scope.status = "Audio files cleared.";
+        });      
     };
+
+//     $scope.checkAll = function(e) {
+//       e.preventDefault();
+//       $scope.toDownload = $scope.availableFiles.map(function(item) item.filepath);
+//       $scope.status = "All files selected.";
+//     };
+// 
+//     $scope.checkNone = function(e) {
+//       e.preventDefault();
+//       $scope.toDownload = [];
+//       $scope.status = "Files to download cleared.";
+//     };
     
-    $scope.download = function() {
-      $http({
-        method: 'GET',
-        url: 'fetch.php',
-        params: {
-          files: JSON.stringify($scope.toDownload)
-        }
-      }).success(function() {
-        $scope.status = "Files successfully downloaded.";
-      }).error(function() {
-        $scope.status = "There was a problem.";
-      });
-    };      
+//     $scope.download = function() {
+//       $http({
+//         method: 'GET',
+//         url: 'do_it.php',
+//         params: {
+//           action: 'download_files',
+//           "files[]": $scope.toDownload
+//         }
+// //       }).success(function() {
+// //         $scope.status = "Files successfully downloaded.";
+// //       }).error(function() {
+// //         $scope.status = "There was a problem.";
+//       });
+//     };      
   });
         
